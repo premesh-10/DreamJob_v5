40163621f480c282903b52b0c69d6a16e22d258d
premesh-10 <166977910+premesh-10@users.noreply.github.com>
2026-08-24 00:21:38 +0530

    Updated url

--- backend/server.js
+++ backend/server.js
@@ -123,7 +123,7 @@ app.use(express.json({
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: ['https://dream-job-v5.vercel.app'],
     credentials: true,
 }));
 
